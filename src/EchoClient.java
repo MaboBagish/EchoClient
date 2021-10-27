6d6fda810c130aa9e304a5655c3e4cfa7c5d7e38
trest <trestcontra@bk.ru>
2021-10-27 16:38:47 +0600

    vtoroe
    vtoroe zavershenie

--- src/EchoClient.java
+++ src/EchoClient.java
@@ -1,6 +1,5 @@
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
@@ -23,8 +22,23 @@ public class EchoClient {
 
     public void run (){
         System.out.println ("напиши 'bye' что бы выйти %n%n%n" );
+        try (ServerSocket server = new ServerSocket (  port )) {
+            try (Socket clientSocket = server.accept ( )){
+                handle (clientSocket);
+            }
+
+        }catch (IOException e) {
+            String formatMsg = "Вероятнеее всего порт %s занят. %n";
+            System.out.printf (formatMsg, port);
+            e.printStackTrace ();
+        }
+
+
+
+
 
         try (Socket socket = new Socket ( host, port )) {
+
             Scanner scanner = new Scanner (System.in, "UTF-8");
             OutputStream output = socket.getOutputStream ();
             PrintWriter writer = new PrintWriter (output);
@@ -51,6 +65,20 @@ public class EchoClient {
             String msg = "Can't connect to %s:%s!%n";
             System.out.printf (msg, host, port);
             e.printStackTrace ();
+        }
+    }
+    private void handle (Socket socket) throws IOException {
+        InputStream input = socket.getInputStream ();
+        InputStreamReader isr = new InputStreamReader (input, "UTF-8");
+
+        try (Scanner scanner = new Scanner (isr)) {
+            while (true){
+                String message = scanner.nextLine ().trim ();
+
+
+                System.out.printf ("Got: %s%n", message );
+
+            }
         }
     }
 
