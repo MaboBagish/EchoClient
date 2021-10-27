8c03bbd56797d11089438fb1b6720603456d4339
trest <trestcontra@bk.ru>
2021-10-27 11:31:07 +0600

    vtoroe

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -1,2 +1,76 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class EchoServer {
+
+    private final  String host;
+    private final  int port;
+    private final  String END_WORLD = "bye";
+    private EchoServer(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+
+
+    static EchoServer bindToPort ( int port) {
+        String localhost = "127.0.0.1";
+        return new EchoServer (localhost, port);
+    }
+
+
+    public void run (){
+
+        try (ServerSocket server = new ServerSocket (  port )) {
+            try (Socket clientSocket = server.accept ( )){
+                handle (clientSocket);
+            }
+
+        }catch (IOException e) {
+            String formatMsg = "Вероятнеее всего порт %s занят. %n";
+            System.out.printf (formatMsg, port);
+            e.printStackTrace ();
+        }
+    }
+
+    private void handle (Socket socket) throws IOException {
+        InputStream input = socket.getInputStream ();
+        InputStreamReader isr = new InputStreamReader (input, "UTF-8");
+
+        try (Scanner scanner = new Scanner (isr)) {
+            while (true){
+                String message = scanner.nextLine ().trim ();
+                String messageReverce = new StringBuffer(message).reverse().toString();
+                System.out.printf ("Got: %s%n", message );
+
+                OutputStream output = socket.getOutputStream ();
+                PrintWriter writer = new PrintWriter (output);
+
+
+
+
+                writer.write (messageReverce);
+                writer.write (System.lineSeparator ());
+                writer.flush ();
+
+
+
+
+
+
+
+                if (END_WORLD.equals (message.toLowerCase ( ))) {
+                    System.out.printf ( "Bye Bye!%n" );
+                    return;
+                }
+            }
+        }
+        catch (NoSuchElementException ex) {
+            System.out.printf ("Clientdropped the connection! ");
+        }
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     private static  Integer PORT = 5555;
     public static void main(String[] args) {
-	final EchoClient echoClient = EchoClient.connectTo ( PORT);
-	echoClient.run ();
-        EchoClient.connectTo (8788).run ();
+        Scanner sc =new Scanner (System.in);
+        final EchoClient echoClient = EchoClient.connectTo ( PORT);
+        final EchoServer echoServer = EchoServer.bindToPort (PORT);
+        System.out.println ("Для отправки сообщения нажмите 1\n" +
+                "Для према сообщения нажмите 2" );
+
+        int mess = sc.nextInt ();
+
+
+
+
+
+	if(mess ==1){
+        echoClient.run ();
+	    EchoClient.connectTo (8788).run ();
+	}else if(mess ==2) {
+	    echoServer.run();
+        EchoServer.bindToPort (8788).run ( );
+    }else System.out.println ("Неправильный набор" );
     }
 }
