af059ec334faa78e2f45f632513a7d3086c6419f
trest <trestcontra@bk.ru>
2021-11-01 15:00:38 +0600

    vtoroe
    vtoroe zavershenie
    
    vse tri
    
    swing

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -1,76 +1,138 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Locale;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class EchoServer {
 
-    private final  String host;
-    private final  int port;
-    private final  String END_WORLD = "bye";
+    private final String host;
+    private final int port;
+    private final String END_WORLD = "bye";
+    private final String VREMYA = "date";
+    private final String HHMMSS = "time";
+    private final String REVERCE = "reverce";
+    private final String UP = "upper";
+    private final String TEXT = "sms";
+    private final ExecutorService pool = Executors.newCachedThreadPool ( );
+
     private EchoServer(String host, int port) {
         this.host = host;
         this.port = port;
     }
 
 
-
-    static EchoServer bindToPort ( int port) {
+    static EchoServer bindToPort(int port) {
         String localhost = "127.0.0.1";
         return new EchoServer (localhost, port);
     }
 
 
-    public void run (){
+    public void run() {
 
-        try (ServerSocket server = new ServerSocket (  port )) {
-            try (Socket clientSocket = server.accept ( )){
-                handle (clientSocket);
+        try (ServerSocket server = new ServerSocket (port)) {
+
+            while (!server.isClosed ( )) {
+                Socket clientSocket = server.accept ( );
+                pool.submit (() -> handle (clientSocket));
+                pool.submit (()->handleX (clientSocket));
             }
 
-        }catch (IOException e) {
+        } catch (IOException e) {
             String formatMsg = "Вероятнеее всего порт %s занят. %n";
             System.out.printf (formatMsg, port);
-            e.printStackTrace ();
+            e.printStackTrace ( );
         }
     }
 
-    private void handle (Socket socket) throws IOException {
-        InputStream input = socket.getInputStream ();
-        InputStreamReader isr = new InputStreamReader (input, "UTF-8");
-
-        try (Scanner scanner = new Scanner (isr)) {
-            while (true){
-                String message = scanner.nextLine ().trim ();
-                String messageReverce = new StringBuffer(message).reverse().toString();
-                System.out.printf ("Got: %s%n", message );
-
-                OutputStream output = socket.getOutputStream ();
-                PrintWriter writer = new PrintWriter (output);
-
-
-
-
-                writer.write (messageReverce);
-                writer.write (System.lineSeparator ());
-                writer.flush ();
-
-
-
-
-
-
-
-                if (END_WORLD.equals (message.toLowerCase ( ))) {
-                    System.out.printf ( "Bye Bye!%n" );
-                    return;
+    private void handle(Socket socket) {
+        System.out.printf ("Client connected: %s\n", socket);
+        try (socket;
+             Scanner reader = getReader (socket);
+             PrintWriter writer = getWriter (socket))
+        {
+            sendResponse ("Привет" + socket, writer);
+            while (true) {
+                String message = reader.nextLine ( );
+                System.out.println (message );
+                if (isEmptyMsg (message) || isQuitMsg (message)) {
+                    break;
                 }
             }
+
+        } catch (NoSuchElementException ex) {
+        System.out.printf ("Client closed: %s\n", socket);
+    }catch(
+    IOException e)
+    {
+        e.printStackTrace ( );
+    }
+System.out.printf ("Client disconnected: %s\n", socket);
+}
+
+    private void handleX(Socket socket) {
+        System.out.printf ("Client connected: %s\n", socket);
+        try (socket;
+             Scanner reader = getReader (socket);
+             PrintWriter writer = getWriter (socket))
+        {
+            sendResponse ("Привет" + socket, writer);
+            while (true) {
+                Scanner scanner = new Scanner (System.in);
+                String sms = scanner.nextLine ();
+                sendResponse (sms, writer);
+                if (isEmptyMsg (sms) || isQuitMsg (sms)) {
+                    break;
+                }
+            }
+
+        } catch (NoSuchElementException ex) {
+            System.out.printf ("Client closed: %s\n", socket);
+        }catch(
+                IOException e)
+        {
+            e.printStackTrace ( );
         }
-        catch (NoSuchElementException ex) {
-            System.out.printf ("Clientdropped the connection! ");
-        }
+        System.out.printf ("Client disconnected: %s\n", socket);
+    }
+
+
+
+
+
+
+    private void sendResponse(String response, PrintWriter writer) {
+        writer.write (response);
+        writer.write (System.lineSeparator ());
+        writer.flush ();
 
     }
+
+    private boolean isQuitMsg(String message) {
+        return "bye".equalsIgnoreCase (message);
+    }
+
+    private boolean isEmptyMsg(String message) {
+        return  message == null || message.isBlank ();
+    }
+
+    private PrintWriter getWriter(Socket socket) throws IOException {
+        OutputStream stream = socket.getOutputStream ();
+        return new PrintWriter (stream);
+    }
+
+    private Scanner getReader(Socket socket) throws IOException {
+        InputStream stream = socket.getInputStream ();
+        InputStreamReader input = new InputStreamReader (stream,"UTF-8");
+        return  new Scanner (input);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,23 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    private static  Integer PORT = 5555;
-    public static void main(String[] args) {
+    private static  Integer PORT = 8788;
+    public static void main(String[] args) throws IOException {
         Scanner sc =new Scanner (System.in);
-        final EchoClient echoClient = EchoClient.connectTo ( PORT);
-        final EchoServer echoServer = EchoServer.bindToPort (PORT);
-        System.out.println ("Для отправки сообщения нажмите 1\n" +
-                "Для према сообщения нажмите 2" );
-
-        int mess = sc.nextInt ();
+//        final EchoClient echoClient = EchoClient.connectTo ( PORT);
+//
+//
+//        echoClient.run ();
 
 
 
+        final EchoServer echoServer = EchoServer.bindToPort  ( PORT);
+
+        echoServer.run ();
 
 
-	if(mess ==1){
-        echoClient.run ();
-	    EchoClient.connectTo (8788).run ();
-	}else if(mess ==2) {
-	    echoServer.run();
-        EchoServer.bindToPort (8788).run ( );
-    }else System.out.println ("Неправильный набор" );
+
+
     }
 }
